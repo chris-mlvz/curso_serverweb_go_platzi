085f18d7a61676c74aa323308903ea50a5838bd1
chris-mlvz <chris.mlvz@gmail.com>
2021-08-30 10:19:16 -0500

    go for

--- concurrency/go_servers.go
+++ concurrency/go_servers.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
-	start := time.Now()
+	// start := time.Now()
 	ch := make(chan string, 4)
 
 	servers := []string{
@@ -16,25 +15,26 @@ func main() {
 		"http://facebook.com",
 		"http://instagram.com",
 	}
-	for _, server := range servers {
-		go checkServer(server, ch)
+
+	for {
+		for _, server := range servers {
+			go checkServer(server, ch)
+		}
+		select {
+		case v := <-ch:
+			fmt.Println(v)
+		}
 	}
 
-	for range servers {
-		fmt.Println(<-ch)
-	}
-
-	elapsed := time.Since(start)
-	fmt.Println("Time elapsed:", elapsed)
+	// elapsed := time.Since(start)
+	// fmt.Println("Time elapsed:", elapsed)
 }
 
 func checkServer(server string, ch chan<- string) {
 	_, err := http.Get(server)
 	if err != nil {
-		fmt.Println(server, "is not available")
-		ch <- "error"
+		ch <- server + " is not available"
 		return
 	}
-	fmt.Println(server, "is working normally")
-	ch <- "ok"
+	ch <- server + " is working normally"
 }
